5c05563ae5e8d0e116032f9d9c257eab06940e04
Rafael Lopez <lpz.raf@gmail.com>
2019-11-03 11:09:13 -0500

    It will add cors gem and namespaces in routes file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :name, :body, presence: true 
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 500 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     has_many :comments
+    validates :title, :body, presence: true 
 end
